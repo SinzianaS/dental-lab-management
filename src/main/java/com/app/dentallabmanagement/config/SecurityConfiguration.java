5ed86e0b580bc5014590548091e0dcf1b49d63c5
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2023-02-25 20:16:12 +0200

    added custom JWT filter to Spring Security

--- src/main/java/com/app/dentallabmanagement/config/SecurityConfiguration.java
+++ src/main/java/com/app/dentallabmanagement/config/SecurityConfiguration.java
@@ -1,16 +1,17 @@
 package com.app.dentallabmanagement.config;
 
-import com.app.dentallabmanagement.service.UserDetailServiceImpl;
+import com.app.dentallabmanagement.filter.JwtFilter;
 import com.app.dentallabmanagement.util.CustomPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -18,6 +19,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private UserDetailsService userDetailsService;
     @Autowired
     private CustomPasswordEncoder customPasswordEncoder;
+    @Autowired
+    private JwtFilter JwtFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -27,9 +30,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-    }
+        http = http.csrf().disable().cors().disable();
 
+        //using a stateless policy authentication
+        http = http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+
+        http = http.exceptionHandling()
+                .authenticationEntryPoint((request, response, exception) -> {
+                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());       //sending 4XX responses code
+
+                }).and();
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated();
+
+
+        http.addFilterBefore(JwtFilter, UsernamePasswordAuthenticationFilter.class);  //use our custom created filter before the default filter
+    }
 }
 
 
--- src/main/java/com/app/dentallabmanagement/service/UserDetailServiceImpl.java
+++ src/main/java/com/app/dentallabmanagement/service/UserDetailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.app.dentallabmanagement.service;
 
 import com.app.dentallabmanagement.entity.User;
+import com.app.dentallabmanagement.repository.UserRepository;
 import com.app.dentallabmanagement.util.CustomPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,19 +9,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
     @Autowired
-    private CustomPasswordEncoder customPasswordEncoder;
+    private UserRepository userRepository;
 
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(customPasswordEncoder.getPasswordEncoder().encode("asdfasdf"));
-        user.setId(1L);
-        return user;
+
+        Optional<User> userOpt = userRepository.findByUsername(username);   //querying the database using the repository
+
+        return userOpt.orElseThrow(()-> new UsernameNotFoundException("Invalid credentials."));
     }
 }
